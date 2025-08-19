6b22a021d161609de8b7d6424209b497ee9f161c
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-08-19 17:25:59 +0530

    server setup

--- backend/src/App.js
+++ backend/src/App.js
@@ -23,7 +23,7 @@ app.use("/api/v1/users", userRoutes);
 const start = async () => {
   app.set("mongo_user");
   const connectionDb = await mongoose.connect(
-    "mongodb+srv://pushpendra:zoom123@cluster0.9lk1dov.mongodb.net/"
+    "mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/"
   );
 
   console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`);
